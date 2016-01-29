6167218010a9d8f617b0a44934d0fa525762fcdc
eggoez <eggoez@gmail.com>
2016-01-29 07:41:22 +0700

    + big bold

---
+++ asset/js/bbold.js
@@ -0,0 +1,36 @@
+var normal="abcdefghijklmnopqrstuvwxyz0123456789!#%&'()*+,-./:;<=>?@[\]^_"
+   var changed="ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ０１２３４５６７８９！＃％＆＇（）＊＋，－．／：；＜＝＞？＠［＼］＾＿"
+   
+   
+   function change(_in, _out)
+   {
+     var s="";
+     var n=_in.value.toLowerCase();
+   
+     ///if (_in.value.length=="0") _arab.value="";
+     
+     for(i=0; i<n.length; i++)
+     {
+       var c=n.charAt(i);
+       for(j=0; (j<normal.length)&&(c!=normal.charAt(j)); j++);
+       if (j<normal.length) { 
+         s+=changed.charAt(j);} else {
+         s+=c;
+       }
+     }
+   
+     _out.value=s;
+   
+   }
+   
+   function focusFirst() {
+   
+     if (els = oTD.getElementsByTagName("input")) {
+       els[0].focus();
+     }
+   }
+   
+   function highlight(field) {
+   field.focus();
+     field.select();
+   }
