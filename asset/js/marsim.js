91572d2973e18454ff01aefc9a99a76fe20ae55c
Anonymous <cipb3@yahoo.com>
2017-10-27 15:04:36 +0700

    Fri, Oct 27, 2017  3:04:35 PM

--- asset/js/marsim.js
+++ asset/js/marsim.js
@@ -1,4 +1,4 @@
-dN=["Minggu","Senin","Selasa","Rabu","Kamis","Jum'at","Sabtu"];
+dN=["Minggu","Senen","Seloso","Rebo","Kemis","Jemuah","Sebtu"];
 mN=["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","Nopember","Desember"];
 pS=["Legi","Pahing","Pon","Wage","Kliwon"];
 sT=["font-size:","<tr>","</tr>"];
