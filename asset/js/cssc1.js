2d246afdb9101ea169f00046a5671d536f8132c9
Anonymous <eggoez@yandex.com>
2017-11-09 08:39:37 +0700

    Thu, Nov 09, 2017  8:39:36 AM

--- asset/js/cssc1.js
+++ asset/js/cssc1.js
@@ -1,1 +1,31 @@
-var csssrc=document.getElementById('csssrc'),cssdst=document.getElementById('cssdst'),slength=document.getElementById('slength'),dlength=document.getElementById('dlength'),result=document.getElementById('result');function updateStat(){if(csssrc.value.length&&cssdst.value.length){slength.innerHTML=csssrc.value.length;dlength.innerHTML=cssdst.value.length;result.innerHTML=(100*cssdst.value.length/csssrc.value.length).toFixed(2)}else{slength.innerHTML=0;dlength.innerHTML=0;result.innerHTML=0}document.getElementById('stat').style.display='block'}document.getElementById('minimize').onclick=function(){var compressor=new CSSOCompressor(),translator=new CSSOTranslator();if(false===document.getElementById('o_colors').checked){delete compressor.defCCfg.compressColor}else{compressor.defCCfg['compressColor']=1}if(false===document.getElementById('o_fweight').checked){delete compressor.defCCfg.compressFontWeight}else{compressor.defCCfg['compressFontWeight']=1}cssdst.value=translator.translate(cleanInfo(compressor.compress(srcToCSSP(csssrc.value,'stylesheet',true))));cssdst.style.display='block';updateStat()};
+var csssrc = document.getElementById('csssrc'), cssdst = document.getElementById('cssdst'),
+					slength = document.getElementById('slength'), dlength = document.getElementById('dlength'),
+					result = document.getElementById('result');
+					function updateStat() {
+						if (csssrc.value.length && cssdst.value.length) {
+							slength.innerHTML = csssrc.value.length;
+							dlength.innerHTML = cssdst.value.length;
+							result.innerHTML = (100 * cssdst.value.length / csssrc.value.length).toFixed(2);
+						} else {
+							slength.innerHTML = 0;
+							dlength.innerHTML = 0;
+							result.innerHTML = 0;
+						}
+						document.getElementById('stat').style.display = 'block';
+					}
+					document.getElementById('minimize').onclick = function() {
+						var compressor = new CSSOCompressor(), translator = new CSSOTranslator();
+						if (false === document.getElementById('o_colors').checked) {
+							delete compressor.defCCfg.compressColor;
+						} else {
+							compressor.defCCfg['compressColor'] = 1;
+						}
+						if (false === document.getElementById('o_fweight').checked) {
+							delete compressor.defCCfg.compressFontWeight;
+						} else {
+							compressor.defCCfg['compressFontWeight'] = 1;
+						}
+						cssdst.value = translator.translate(cleanInfo(compressor.compress(srcToCSSP(csssrc.value, 'stylesheet', true))));
+						cssdst.style.display = 'block';
+						updateStat();
+					};
